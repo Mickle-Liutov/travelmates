6cae7f48450bc132f47d7fecb2c7ab78905cbb8d
Michael <m.liutov.dev@gmail.com>
2022-01-08 19:07:11 +0100

    Implement login screen

--- app/src/main/java/cz/cvut/fit/travelmates/explore/ExploreViewModel.kt
+++ app/src/main/java/cz/cvut/fit/travelmates/explore/ExploreViewModel.kt
@@ -2,32 +2,37 @@ package cz.cvut.fit.travelmates.explore
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import cz.cvut.fit.travelmates.authapi.data.CredentialsDataSource
+import cz.cvut.fit.travelmates.authapi.AuthRepository
 import cz.cvut.fit.travelmates.core.livedata.SingleLiveEvent
 import cz.cvut.fit.travelmates.core.livedata.immutable
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ExploreViewModel @Inject constructor(private val credentialsDataSource: CredentialsDataSource) :
-    ViewModel() {
+class ExploreViewModel @Inject constructor(
+    private val authRepository: AuthRepository
+) : ViewModel() {
+
+    val idToken = MutableStateFlow("")
 
     private val _eventNavigateAuth = SingleLiveEvent<Unit>()
     val eventNavigateAuth = _eventNavigateAuth.immutable()
 
     init {
-        startCredentialsCheck()
+        checkSession()
     }
 
-    private fun startCredentialsCheck() {
+    private fun checkSession() {
         viewModelScope.launch {
-            credentialsDataSource.hasValidCredentials().collect { hasCredentials ->
-                if (!hasCredentials) {
-                    _eventNavigateAuth.call()
-                }
+            val hasSession = authRepository.hasValidSession()
+            if (!hasSession) {
+                _eventNavigateAuth.call()
+                return@launch
             }
+            val token = authRepository.getIdToken()
+            idToken.value = token
         }
     }
 
