7061eb7b942fbcfa4aa051e3605c8d303d46ac03
Michael <m.liutov.dev@gmail.com>
2022-04-17 20:11:05 +0200

    Redesign review and reject requests

--- trips/src/main/java/cz/cvut/fit/travelmates/trips/request/review/RequestViewModel.kt
+++ trips/src/main/java/cz/cvut/fit/travelmates/trips/request/review/RequestViewModel.kt
@@ -1,11 +1,13 @@
-package cz.cvut.fit.travelmates.trips.request
+package cz.cvut.fit.travelmates.trips.request.review
 
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
 import androidx.lifecycle.viewModelScope
 import cz.cvut.fit.travelmates.core.coroutines.launchCatching
 import cz.cvut.fit.travelmates.core.livedata.SingleLiveEvent
 import cz.cvut.fit.travelmates.core.livedata.immutable
+import cz.cvut.fit.travelmates.trips.request.RequestsRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import timber.log.Timber
 import javax.inject.Inject
@@ -19,13 +21,17 @@ class RequestViewModel @Inject constructor(
     private val _eventAccepted = SingleLiveEvent<Unit>()
     val eventAccepted = _eventAccepted.immutable()
 
-    private val _eventRejected = SingleLiveEvent<Unit>()
-    val eventRejected = _eventRejected.immutable()
+    private val _eventNavigateReject = SingleLiveEvent<Long>()
+    val eventNavigateReject = _eventNavigateReject.immutable()
 
     private val _eventNavigateBack = SingleLiveEvent<Unit>()
     val eventNavigateBack = _eventNavigateBack.immutable()
 
     private val args = RequestFragmentArgs.fromSavedStateHandle(savedStateHandle)
+    private val request = args.request
+    val senderImage = liveData { emit(request.user.picture) }
+    val senderName = liveData { emit(request.user.name) }
+    val message = liveData { emit(request.message) }
 
     fun onAcceptPressed() {
         viewModelScope.launchCatching(execute = {
@@ -39,14 +45,7 @@ class RequestViewModel @Inject constructor(
     }
 
     fun onRejectPressed() {
-        viewModelScope.launchCatching(execute = {
-            requestsRepository.rejectRequest(args.request.id, "TODO reason", true) /*TODO Add*/
-            _eventRejected.call()
-            _eventNavigateBack.call()
-        }, catch = {
-            Timber.e(it)
-            //TODO
-        })
+        _eventNavigateReject.value = args.request.id
     }
 
     fun onBackPressed() {
