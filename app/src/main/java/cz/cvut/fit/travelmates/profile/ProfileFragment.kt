f472b78f485d755cd48e2f38bed0511a277fd023
Michael <m.liutov.dev@gmail.com>
2022-04-18 16:39:27 +0200

    Fix back navigation

--- app/src/main/java/cz/cvut/fit/travelmates/profile/ProfileFragment.kt
+++ app/src/main/java/cz/cvut/fit/travelmates/profile/ProfileFragment.kt
@@ -62,7 +62,7 @@ class ProfileFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         setupObservers()
-        requireActivity().onBackPressedDispatcher.addCallback {
+        requireActivity().onBackPressedDispatcher.addCallback(viewLifecycleOwner) {
             viewModel.onBackPressed()
         }
     }
